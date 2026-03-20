60e4bec7cf97c999cfe5b6f05e0d102e522a70a2
Sergeyi <Schwarz@S_Schwarz.com>
2026-03-20 14:36:56 +0300

    Добавлен метод get_category в Product

--- src/models/product.py
+++ src/models/product.py
@@ -16,6 +16,9 @@ class Product:
             raise ValidationError("Цена не может быть отрицательной")
         self.price = price
 
+    def get_category(self):
+        pass
+
     def check_stock(self):
         pass
 
